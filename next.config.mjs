4484201897db9c04bfca36c101bd854551612f84
Manimohan R <56622784+ManiMohan-Radhakrishnan@users.noreply.github.com>
2024-12-04 18:43:34 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Custom Webpack configuration here
+    return config;
+  },
+  // Optionally, you can tell Next.js to use 'src' as the base directory for pages
+  // This is useful when using a src-based structure
+  experimental: {
+    srcDir: 'src', // Tells Next.js to look for 'src/pages'
+  },
 };
 
 export default nextConfig;
+
